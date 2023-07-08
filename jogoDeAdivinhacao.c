cf3f44cda01a8e044a41d1d88af82f19e28e488d
yrnThiago <thiagodias_123@hotmail.com>
2023-07-08 11:21:30 -0300

    feat: add conditionals, add loops

--- jogoDeAdivinhacao.c
+++ jogoDeAdivinhacao.c
@@ -1,15 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define NUMERO_DE_TENTATIVAS 5
+
 int main(void){
     printf("******************************************\n");
     printf("* Bem vindo ao nosso jogo de adivinhacao *\n");
     printf("******************************************\n");
 
-    int numeroSecreto = 42;
+    int numerosecreto = 42;
     int chute;
+    int tentativas = 1;
 
-    printf("Qual eh o seu chute? ");
-    scanf("%d", &chute);
-    printf("Seu chute foi: %d\n", chute);
+    while(1)
+    {
+        printf("Tentativa %d\n", tentativas);
+        printf("Qual eh o seu chute? ");
+        scanf("%d", &chute);
+        printf("Seu chute foi: %d\n", chute);
+
+        if(chute < 0){
+            printf("Voce nao pode chutar numeros negativos!\n");
+            continue;
+        }
+
+        int acertou = (chute == numerosecreto);
+        int maior = (chute > numerosecreto);
+
+        if (acertou)
+        {
+            printf("Parabens! voce acertou em %d tentativas!\n", tentativas);
+            printf("Jogue de novo, voce eh um bom jogador!\n");
+            break;
+        } 
+        else if (maior){
+            printf("Seu chute foi maior que o numero secreto!\n");
+        }
+        else {
+            printf("Seu chute foi menor que o numero secreto!\n");
+        }
+
+        tentativas++;
+    }
+
+    printf("Fim de jogo!");
+
 }
