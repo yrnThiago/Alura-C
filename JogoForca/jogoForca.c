d30219ae6b0afb8eb9f514af8632a630db16b624
yrnThiago <thiagodias_123@hotmail.com>
2023-07-11 20:27:57 -0300

    feat: add pointers

--- JogoForca/jogoForca.c
+++ JogoForca/jogoForca.c
@@ -1,53 +1,66 @@
 #include <stdio.h>
 #include <string.h>
 
+char palavrasecreta[20];
+char chutes[26];
+int tentativas = 0;
+
 void abertura(){
     printf("/****************/\n");
     printf("/ Jogo de Forca */\n");
     printf("/****************/\n\n");
 }
 
-void chuta(char chutes[26], int tentativas){
+void chuta(){
     char chute;
     scanf(" %c", &chute);
 
     chutes[tentativas] = chute;
+    tentativas++;
+}
+
+int jachutou(char letra){
+    int achou = 0;
+
+    for (int j = 0; j < tentativas; j++) {
+        if (chutes[j] == letra){
+            achou = 1;
+            break;
+        }
+    }
+
+    return achou;
+}
+
+void desenhaforca() {
+    for (int i = 0; i < strlen(palavrasecreta); i++) {
+
+        int achou = jachutou(palavrasecreta[i]);
+        if (achou){
+            printf("%c ", palavrasecreta[i]);
+        } else {
+            printf("_ ");
+        }
+        
+    }
+    putchar('\n');
+}
+
+void escolhepalavra(char palavrasecreta[20]){
+    sprintf(palavrasecreta, "MELANCIA");
 }
 
 int main() {
-    char palavrasecreta[20];
-
-    sprintf(palavrasecreta, "MELANCIA");
-
     int acertou = 0;
     int enforcou = 0;
 
-    char chutes[26];
-    int tentativas = 0;
-
+    escolhepalavra(palavrasecreta);
     abertura();
 
     do {
-        for (int i = 0; i < strlen(palavrasecreta); i++) {
 
-            int achou = 0;
-            for (int j = 0; j < tentativas; j++) {
-                if (chutes[j] == palavrasecreta[i]){
-                    achou = 1;
-                    break;
-                }
-            }
-            if (achou){
-                printf("%c ", palavrasecreta[i]);
-            } else {
-                printf("_ ");
-            }
-            
-        }
-        putchar('\n');
-
-        chuta(chutes, tentativas);
-        tentativas++;
+        desenhaforca();
+        chuta();
 
     } while (!acertou && !enforcou);
 
